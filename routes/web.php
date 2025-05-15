55b6de24674074e807acfea90b284ad6ea033325
RafRizu <thisrafi10@gmail.com>
2025-05-15 11:59:41 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::redirect('/', '/login');
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
