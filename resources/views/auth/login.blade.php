592a7da61951006a40d9215f1156c95c3b200044
Hamadah <muhammadrozaan@gmail.com>
2025-05-15 11:31:25 +0700

    feat(login): using SbAdmin Login Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,26 +1,71 @@
 @extends('layouts.app')
-
 @section('content')
-<h2>Login</h2>
+<div class="container  ">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
 
-@if ($errors->any())
-    <div style="color:red;">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+        <div class="col-xl-10 col-lg-12 col-md-9">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                </div>
+
+                                <form class="user" method="post" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <input type="text" name="username" class="form-control form-control-user"
+                                            id="exampleInputEmail" placeholder="Enter Username...">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" name="password" class="form-control form-control-user"
+                                            id="exampleInputPassword" placeholder="Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck">
+                                            <label class="custom-control-label" for="customCheck">Remember
+                                                Me</label>
+                                        </div>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Login
+                                    </button>
+                                    @if ($errors->any())
+                                    <div class="text-danger text-center">
+                                        <ul class="p-0 m-0"
+                                            style="list-style: none; font-size: .75rem; display: inline-flex; justify-content: center; align-items: center;">
+                                            @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                    @endif
+                                </form>
+                                <hr>
+                                <div class="text-center">
+                                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                </div>
+                                <div class="text-center">
+                                    <a class="small" href="register.html">Create an Account!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     </div>
-@endif
 
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-    <label>Username</label>
-    <input type="text" name="username" required><br><br>
-
-    <label>Password</label>
-    <input type="password" name="password" required><br><br>
-
-    <button type="submit">Login</button>
-</form>
+</div>
 @endsection
+
+@push('body-class') d-flex min-vh-100 justify-content-center align-items-center bg-gradient-primary @endpush
